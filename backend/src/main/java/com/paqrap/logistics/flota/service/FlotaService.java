1dcdeefa3d098c66904780a0545fb4680a9ff904
Oscar Lau Lu <oscarlau.734@gmail.com>
2026-09-24 19:15:23 -0500

    fix

--- backend/src/main/java/com/paqrap/logistics/flota/service/FlotaService.java
+++ backend/src/main/java/com/paqrap/logistics/flota/service/FlotaService.java
@@ -112,7 +112,7 @@ public class FlotaService {
         u.registrarAveria(guardada);
         
         // Regla FAQ: "Las unidades de transporte en las averías 2 y 3, son llevadas de manera instantánea al almacen central."
-        if (dto.getTipo() == TipoAveria.TIPO_2 || dto.getTipo() == TipoAveria.TIPO_3) {
+        if (dto.getTipo() == com.paqrap.logistics.flota.model.TipoAveria.TIPO_2 || dto.getTipo() == com.paqrap.logistics.flota.model.TipoAveria.TIPO_3) {
             u.setUbicacionActual(new Ubicacion(27, 14));
             // Ojo: Los paquetes no entregados ya son liberados por el AveriaDestroyOperator para ser reasignados desde el almacén
         }
